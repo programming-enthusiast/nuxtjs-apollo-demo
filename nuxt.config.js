1fc8c33bafdeba697a9eefa9464982796bc96465
programming enthusiast <miura.nakamoto@outlook.com>
2020-05-02 16:36:29 +0800

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,12 @@
+export default {
+  mode: "universal",
+  modules: ["@nuxtjs/apollo"],
+  apollo: {
+    clientConfigs: {
+      default: {
+        httpEndpoint:
+          "https://api.graph.cool/simple/v1/cj1dqiyvqqnmj0113yuqamkuu"
+      }
+    }
+  }
+};
